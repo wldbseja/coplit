95a95004d9c87cd2dbaf01a3c6473fe9b62594b4
yooni <jiyoon0616@naver.com>
2023-04-22 21:18:00 +0900

    Section 3 - 23번 문제 풀이

---
+++ Section 3 Daily Coding /23_bubbleSort.js
@@ -0,0 +1,36 @@
+// 문제
+// 정수를 요소로 갖는 배열을 입력받아 오름차순으로 정렬하여 리턴해야 합니다.
+// 버블 정렬(bubble sort)은 여러 정렬 알고리즘(삽입 정렬, 퀵 정렬, 병합 정렬, 기수 정렬 등) 중 가장 기본적인 알고리즘입니다.
+
+const swap = function (idx1, idx2, arr) {
+  // 두 변수를 바꾸는 방법
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+// optimized solution
+let bubbleSort = function (arr) {
+  let N = arr.length;
+
+  for (let i = 0; i < N; i++) {
+    // swap 횟수를 기록한다.
+    // 어떤 요소도 swap되지 않은 경우, 배열은 정렬된 상태이다.
+    let swaps = 0;
+
+    // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+    // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+    for (let j = 0; j < N - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swaps++;
+        swap(j, j + 1, arr);
+      }
+    }
+
+    if (swaps === 0) {
+      break;
+    }
+  }
+
+  return arr;
+};
+
+let output = bubbleSort([2, 1, 3]);
+console.log(output); // --> [1, 2, 3]
